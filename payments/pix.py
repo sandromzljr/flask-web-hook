70280b3e014190a591c77a21c3ba9e6d67b6d197
sandro_mazzolla <jmazzolla94@gmail.com>
2024-03-06 22:24:14 -0300

    FEAT: generate qr code with github url

---
+++ payments/pix.py
@@ -0,0 +1,22 @@
+import uuid
+import qrcode
+
+class Pix:
+    def __init__(self) -> None:
+        pass
+
+    def create_payment(self):
+        # Gerando um uuid aleatório
+        bank_payment_id = str(uuid.uuid4())
+
+        # Gerando um qr code e salvando na pasta static
+        # hash_payment = f"hash_payment_{bank_payment_id}"
+        hash_payment = "https://github.com/sandromzljr/flask-web-hook"
+
+        img = qrcode.make(hash_payment)
+        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+
+        return {
+            "bank_payment_id": bank_payment_id,
+            "qr_code_path": f"qr_code_payment_{bank_payment_id}"
+        }
